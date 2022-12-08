6ea49e9f0071bbce4cdd95831be87830dac19b9e
Chuanyang Gong <cy_gong@foxmail.com>
2022-12-08 16:18:45 +0800

    complete login client

--- client/client.go
+++ client/client.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"go_grpc_practice/proto"
 
+	"google.golang.org/grpc"
+)
+
+func main() {
+	client := client{}
+	client.init()
+
+	// 测试错误的用户名
+	client.Login("root1", "root123")
+
+	// 测试密码错误
+	client.Login("root", "root1234")
+
+	// 测试成功登录
+	client.Login("root", "root123")
+
+	client.close()
+}
+
+type client struct {
+	conn  *grpc.ClientConn
+	token string
+	c     proto.LoginerClient
+}
+
+func (c *client) init() {
+	conn, err := grpc.Dial("127.0.0.1:8080", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	c.c = proto.NewLoginerClient(conn)
+	c.conn = conn
+}
+
+func (c *client) close() {
+	c.conn.Close()
+}
+
+func (c *client) Login(username string, password string) {
+	r, err := c.c.Login(context.Background(), &proto.LoginReqData{
+		Password: password,
+		Username: username,
+	})
+	if err != nil {
+		panic(err)
+	}
+	if r.Success {
+		c.token = r.Token
+		fmt.Println("Token: " + r.Token)
+	}
+	fmt.Println(r.Msg)
 }
--- server/server.go
+++ server/server.go
@@ -3,12 +3,28 @@ package main
 import (
 	"context"
 	"math/rand"
+	"net"
 	"strconv"
 	"strings"
 	"time"
 
 	"go_grpc_practice/proto"
+
+	"google.golang.org/grpc"
 )
+
+func main() {
+	g := grpc.NewServer()
+	proto.RegisterLoginerServer(g, newServer())
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+	err = g.Serve(listener)
+	if err != nil {
+		panic(err)
+	}
+}
 
 type server struct {
 	users  map[string]string
